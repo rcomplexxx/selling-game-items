c0f4e1487e2dc35e6af005ffd8f7f351902ec9ed
unknown <rcomplexx@gmail.com>
2023-05-11 00:43:43 +0200

    Updates

---
+++ precache-manifest.934cec03d39bddf4f9c2ae3afeb0d285.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c242b5525057f4f03d4a37d174361b4c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a67e34b4b4ac291ca9cc",
+    "url": "/react-gh-pages/static/css/main.a57e1d9a.chunk.css"
+  },
+  {
+    "revision": "8736f947af9c96365310",
+    "url": "/react-gh-pages/static/js/2.efe60923.chunk.js"
+  },
+  {
+    "revision": "302dbb25a8c6114478c154c6328e20ea",
+    "url": "/react-gh-pages/static/js/2.efe60923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67e34b4b4ac291ca9cc",
+    "url": "/react-gh-pages/static/js/main.4bc8b7b4.chunk.js"
+  },
+  {
+    "revision": "2d13745eb750197f360e",
+    "url": "/react-gh-pages/static/js/runtime-main.e09cdf97.js"
+  },
+  {
+    "revision": "9e8c568f2e6ce6efcfed9c87215cc9e9",
+    "url": "/react-gh-pages/static/media/1.9e8c568f.png"
+  },
+  {
+    "revision": "80da09910fc7b6bdb521a5d220c2d16a",
+    "url": "/react-gh-pages/static/media/3.80da0991.png"
+  },
+  {
+    "revision": "07fe1645690ea1f892278fbb378de29e",
+    "url": "/react-gh-pages/static/media/4.07fe1645.png"
+  },
+  {
+    "revision": "c296eb948b886d937d7ee972dac59e3d",
+    "url": "/react-gh-pages/static/media/5.c296eb94.png"
+  },
+  {
+    "revision": "db77e20c727635545a284ab5bcb4e0ef",
+    "url": "/react-gh-pages/static/media/ashewallpaper.db77e20c.png"
+  },
+  {
+    "revision": "5a4c50f1213520590e0a32e437cc0560",
+    "url": "/react-gh-pages/static/media/boxItem.5a4c50f1.png"
+  },
+  {
+    "revision": "880da503b2e27a018ed41728ed1888b1",
+    "url": "/react-gh-pages/static/media/commerce.880da503.png"
+  },
+  {
+    "revision": "f3d4ec9e6bbccd901a0d7618218070e8",
+    "url": "/react-gh-pages/static/media/gamerwp.f3d4ec9e.png"
+  },
+  {
+    "revision": "f42dfd123004af9bcdf2b27c175f6d9d",
+    "url": "/react-gh-pages/static/media/keyboard.f42dfd12.png"
+  },
+  {
+    "revision": "43b1149dd8a3b45db4d23a965b986d5a",
+    "url": "/react-gh-pages/static/media/ornn.43b1149d.png"
+  },
+  {
+    "revision": "be2b321e7e3dd83d00073a4d510c8421",
+    "url": "/react-gh-pages/static/media/yas.be2b321e.png"
+  }
+]);
